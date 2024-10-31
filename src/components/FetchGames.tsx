a84bcbb7c3f4b3dfa944746088167c61e6bd46c8
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-10-31 10:08:13 +0100

    Create a custom hook to fetch the games

--- src/components/FetchGames.tsx
+++ src/components/FetchGames.tsx
@@ -1,31 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchiGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { useGames } from "./../hooks/useGames";
 
 const FetchGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchiGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err));
-  });
+  const { games, error } = useGames();
   return (
     <>
       {error && <p>Games was not found</p>}
       <ul>
-        {games.map((game) => (
+        {games?.map((game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
