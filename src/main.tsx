7eb89291bf9ad40bdb6aff6118440d39fbd5d332
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-01 07:31:42 +0100

    Implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ChakraBaseProvider, ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
