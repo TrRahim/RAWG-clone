4c7071623c543c3b1764aca80848ac58adbcb324
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-03 13:28:28 +0100

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -31,4 +31,6 @@ export const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+
+    staleTime: 24 * 60 * 60 * 1000,
   });
