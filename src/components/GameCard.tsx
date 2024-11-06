c56a22cc253d8867e7c108b97f941df3f679a0a1
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-06 13:16:06 +0100

    Refactor- move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,18 +1,24 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Link } from "react-router-dom";
+import { Game } from "../entities/Game";
 import { getCroppedImageUrl } from "../services/image-url";
 import CriticScroe from "./CriticScore";
 import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
-import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  const { name, background_image, parent_platforms, metacritic, rating_top, slug } =
-    game;
+  const {
+    name,
+    background_image,
+    parent_platforms,
+    metacritic,
+    rating_top,
+    slug,
+  } = game;
 
   return (
     <Card>
